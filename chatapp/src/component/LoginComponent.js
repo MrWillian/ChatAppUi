ca4845945a87458028da5f3133ac4e83c013cac3
MrWilllian <willian.dev10@gmail.com>
2020-02-09 19:15:11 -0300

    feat: add last changes in LoginScreen

--- chatapp/src/component/LoginComponent.js
+++ chatapp/src/component/LoginComponent.js
@@ -1,23 +1,92 @@
 import React from 'react';
 import {
   View,
-  Text
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+  ImageBackground,
+  Image,
+  TextInput
 } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 export default class LoginComponent extends React.Component {
   render() {
     return(
       <View style={styles.container}>
-        <Text>LoginScreen</Text>
+        <View style={styles.header}>
+          <ImageBackground
+          source={require("../images/header.png")}
+          style={{flex:1, width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center'}}
+          resizeMode={"stretch"}>
+            <View style={styles.logo}>
+              <Image source={require("../images/logo.png")}
+              style={{width:'100%', height: '100%'}} 
+              resizeMode={"stretch"} />
+            </View>
+          </ImageBackground>
+        </View>
+        <View style={styles.footer}>
+          <TextInput style={styles.textInput} placeholder="Username" />
+          <TextInput style={styles.textInput} placeholder="Password" secureTextEntry />
+          <TouchableOpacity onPress={()=>this.props.navigation.navigate('Messages')}>
+            <LinearGradient 
+              start={{x: 0, y: 0}}
+              colors={['#39b54a', '#006837']} style={styles.login}>
+              <Text style={styles.textLogin}>LOGIN</Text>
+            </LinearGradient>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
 }
 
+const { width, height } = Dimensions.get('window');
+const width_logo = width * 0.5;
+const height_logo = width_logo * 1.2;
+const width_textInput = width * 0.8;
+
 var styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    backgroundColor: 'white'
+  },
+  header: {
+    width: '100%',
+    height: '55%'
+  },
+  footer: {
+    width: '100%',
+    height: '45%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo: {
+    width: width_logo,
+    height: height_logo,
+  },
+  textInput: {
+    width: width_textInput,
+    backgroundColor: '#F2F2F2',
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    borderRadius: 50,
+    marginBottom: 15,
+  },
+  login: {
+    width: width_textInput,
+    paddingVertical: 10,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textLogin: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 20
   }
 });
--- chatapp/src/stack/RootStack.js
+++ chatapp/src/stack/RootStack.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { crateAppContainer, createAppContainer } from 'react-navigation';
+import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
 
