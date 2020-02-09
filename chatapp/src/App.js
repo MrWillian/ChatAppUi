2931d4e126ffb50303d8b3f1bf7fc03791e32d69
MrWilllian <willian.dev10@gmail.com>
2020-02-09 18:32:53 -0300

    feat: last changes in structure

--- chatapp/src/App.js
+++ chatapp/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import {
-  View
-} from 'react-native';
+
+import RootStack from './stack/RootStack';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View></View>
+      <RootStack />
     );
   }
 }
